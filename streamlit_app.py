0efe3ef990321947643643b080de06ffe92a25dd
interludevinay <139574707+interludevinay@users.noreply.github.com>
2023-07-14 22:04:31 +0530

    streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,19 @@
+pip install langchain
+
+%%writefile app.py
+
+import streamlit as st
+from langchain.llms import OpenAI
+
+
+# App framework
+openai_api_key = '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'
+st.write('TAU BT')
+prompt = st.text_input('Puch Iss Ki Bhen Ki Aankh')
+
+
+# stuff to the screen
+llm = OpenAI(temperature = 0.9,openai_api_key=openai_api_key)
+if prompt:
+  response = llm(prompt)
+  st.write(response)
